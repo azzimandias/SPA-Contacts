c6c0f749220fce99b5e015dbd2fd07b2ae626f13
Andrew Sharanda <azzimandias@gmail.com>
2022-04-08 16:50:22 +0300

    gh-pages

---
+++ js/chunk-2d0e95df.9b3d6e39.js
@@ -0,0 +1,2 @@
+(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["chunk-2d0e95df"],{"8cdb":function(e,n,t){"use strict";t.r(n);var c=function(){var e=this,n=e.$createElement;e._self._c;return e._m(0)},u=[function(){var e=this,n=e.$createElement,t=e._self._c||n;return t("div",[t("h1",[e._v("Page not found")])])}],r=t("2877"),l={},a=Object(r["a"])(l,c,u,!1,null,null,null);n["default"]=a.exports}}]);
+//# sourceMappingURL=chunk-2d0e95df.9b3d6e39.js.map
